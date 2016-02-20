927a245112fb8e4b7a4d7ad91cd3297bba215bfd
Arvind Rk <arvindsuna10@gmail.com>
2016-02-20 15:45:04 +0530

    Final Crawler along with python file to find maximum accuracy

--- crawler.php
+++ crawler.php
@@ -2,16 +2,16 @@
 include_once('simple_html_dom.php');
 
 $base_url = 'https://play.google.com';
-$target_url = 'https://play.google.com/store/apps/category/GAME_ACTION?hl=en';
+$target_url = 'https://play.google.com/store/apps/category/GAME_STRATEGY';
 
 ini_set('max_execution_time', 300);
 
 $html = new simple_html_dom();
 $html->load_file($target_url);
 
-$url = []; $i=0;
+$url = []; $price_array =[]; $title_array = [];
 
-$dbhost = 'localhost:3310';
+$dbhost = 'localhost:3306';
 $dbuser = 'root';
 $dbpass = 'suna';
 $conn = new mysqli($dbhost, $dbuser, $dbpass);
@@ -30,40 +30,102 @@ if($conn->connect_error) {
 foreach($html->find('a[class=title]') as $link){
 	$value = $base_url.$link->href;
 	array_push($url, $value);
+	$temp_title = $link->title;
+	$temp_title = str_replace( '\'', '', $temp_title );
+	array_push($title_array, $temp_title);
 }
+
+foreach($html->find('div[class=subtitle-container]') as $link1){
+	foreach($link1->find('span[class=display-price]') as $span){
+		$temp_price = scrape($span);
+		if(strcmp($temp_price, "Free") == 0){
+			$temp_price = 0;
+		}
+		else{
+			$temp_dum = (str_replace( 'Rs.', '', $temp_price ));
+			$temp_price = (float)(substr($temp_dum, 2));
+		}
+		array_push($price_array, $temp_price);
+	}
+}
+
 foreach ($url as $nav) {
+	$flag = 0;
 	$html = new simple_html_dom();
-	$html->load_file($nav);		
+	$html->load_file($nav);
 	foreach ($html->find('div[class=id-app-title]') as $val) {
-		echo '<b>'.$val.'</b>'.'<br>';
-		$title = scrape($val);
+		$title = "NULL";
+		$dummmy_1 = scrape($val);
+		$title = str_replace( '\'', '', $dummmy_1 );
+		for ($i=0; $i <count($title_array) ; $i++) { 
+			if(strcmp($title_array[$i], $title) == 0){
+				$price = $price_array[$i];
+			}
+		}
 	}
 	foreach ($html->find('span[class=reviews-num]') as $val1) {
-		//echo "Rating Count : ".$val1.'<br>';
-		$rating_count = scrape($val1);
+		$rating_count = NULL;
+		$dummy = scrape($val1);
+		$rating_count = (int)(str_replace( ',', '', $dummy ));
 	}
 	foreach ($html->find('span[itemprop=genre]') as $val2) {
-		//echo "Genre : ".$val2.'<br>';	
-		$genre = scrape($val2);
+		$genre = "NULL";
+		$dum = scrape($val2);
+		$genre = str_replace( 'amp;', '', $dum );
 	}
 	foreach ($html->find('div[class=score]') as $val3) {
-		//echo "Rating : ".$val3.'<br>';	
-		$rating = scrape($val3);
+		$rating = NULL;
+		$rating = (float)scrape($val3);
 	}
 	foreach ($html->find('div[class=content]') as $value) {
-		$size = "UNAVAILABLE";
-		$price = "Free";
-		echo $value.'<br>';
+		$size = "NULL";
+		//echo $value.'<br>';
 		foreach ($html->find('div[itemprop=datePublished]') as $temp) {
+			$date = "NULL";
 			$date = scrape($temp);
 		}
 		foreach ($html->find('div[itemprop=numDownloads]') as $temp1) {
-			$downloads = scrape($temp1);
+			$t = "NULL";
+			$t = scrape($temp1);
+			if(strcmp($t, " 100,000,000 - 500,000,000 ") == 0)
+				$downloads = "Very High";
+			elseif(strcmp($t, " 500,000,000 - 1,000,000,000 ") == 0)
+				$downloads = "Very High";
+			elseif(strcmp($t, " 10,000,000 - 50,000,000 ") == 0)
+				$downloads = "High";
+			elseif(strcmp($t, " 50,000,000 - 100,000,000 ") == 0)
+				$downloads = "High";
+			elseif(strcmp($t, " 1,000,000 - 5,000,000 ") == 0)
+				$downloads = "Medium";
+			elseif(strcmp($t, " 5,000,000 - 10,000,000 ") == 0)
+				$downloads = "Meduim";
+			elseif(strcmp($t, " 100,000 - 500,000 ") == 0)
+				$downloads = "Low";
+			elseif(strcmp($t, " 500,000 - 1,000,000 ") == 0)
+				$downloads = "Low";
+			else
+				$downloads = "Very Low";
 		}
 		foreach ($html->find('div[itemprop=fileSize]') as $temp2) {
-			$size = scrape($temp2);
+			//$size = "NULL";
+			$temp_size = scrape($temp2);
+			if(strpos($temp_size, 'M') !== FALSE){
+				$size = (float)(str_replace( 'M', '', $temp_size ));
+			}
+			elseif(strpos($temp_size, 'G') !== FALSE){
+				$size = (float)(str_replace( 'G', '000', $temp_size ));
+			}
+			else{
+				$flag = 1;
+				break;
+			}
+		}
+		if($flag){
+			break;
 		}
 	}
+	if($flag)
+		continue;
 
 	$sql = "INSERT INTO games (game_name,genre, rating, review_count, date_Published, size, downloads, price) VALUES ('$title', '$genre', '$rating', '$rating_count', '$date', '$size', '$downloads', '$price')";
 	mysqli_select_db($conn,'dataset');
@@ -73,8 +135,8 @@ foreach ($url as $nav) {
    	if(! $retval ) {
       	die('Could not enter data: ' . mysqli_error($conn));
    	}
-   	echo "Entered data into Database successfully\n";
+   	echo "Entered data into Database successfully".'<br>';
 }
 
-mysql_close($conn);
+//mysql_close($conn);
 ?>
